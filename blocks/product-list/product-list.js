fc8cb4a3448ded7e48f8ce321f926a6822feaeff
Milan Devnath <aarif.islam@kotak.com>
2025-03-19 17:23:39 +0530

    es lint run

--- blocks/product-list/product-list.js
+++ blocks/product-list/product-list.js
@@ -1,11 +1,11 @@
 export default function decorate(block) {
-    console.log(block.children);
-    [...block.children].forEach((row,i) => {
-        [...row.children].forEach((col,j) => {
-            col.classList.add(`product-list-row-${j+1}`);  
-            [...col.children].forEach((item,k) => {
-                item.classList.add(`product-list-item-${k+1}`);
-            })
-        })
-    }
-)}
+  console.log(block.children);
+  [...block.children].forEach((row) => {
+    [...row.children].forEach((col, j) => {
+      col.classList.add(`product-list-row-${j + 1}`);
+      [...col.children].forEach((item, k) => {
+        item.classList.add(`product-list-item-${k + 1}`);
+      });
+    });
+  });
+}
